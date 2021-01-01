54efa51143da6d14955a40d9bd39cacd7324b0b1
Evan Tahler <evantahler@gmail.com>
2021-01-01 11:10:46 -0800

    revert ENV check

--- api/src/config/next.ts
+++ api/src/config/next.ts
@@ -1,8 +1,9 @@
 // learn more about the next.js app options here https://nextjs.org/docs/advanced-features/custom-server
-import { env } from "actionhero";
 
 export const DEFAULT = {
   next: () => {
+    const env = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
+
     return {
       enabled: process.env.NEXT_DISABLED === "true" ? false : true,
       dev: process.env.NEXT_DEVELOPMENT_MODE
